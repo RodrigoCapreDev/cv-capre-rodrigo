3e608af2954c81ce64f59cfdaf586142cb3e615c
rcapredev <rodrigocapre@gmail.com>
2026-05-13 23:23:46 -0300

    feat: add card enter animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,25 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  // activar modo oscuro controlado por la clase "dark"
+
   darkMode: 'class',
   theme: {
     extend: {
+          animation: {
+      'fade-in-up': 'fadeInUp 0.6s ease-out forwards',
+    },
+    keyframes: {
+      fadeInUp: {
+        'from': {
+          opacity: '0',
+          transform: 'translateY(20px)'
+        },
+        'to': {
+          opacity: '1',
+          transform: 'translateY(0)'
+        }
+      }
+    },
       colors: {
         'text': 'var(--text)',
         'secondary-text': 'var(--secondary-text)',
