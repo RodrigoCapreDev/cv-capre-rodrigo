c13a4e58e075ada226819e99cc9bdd3aa55db10d
Rodrigo Capre <rodrigocapre@gmail.com>
2026-05-22 14:43:31 -0300

    change card bg color

--- src/components/LoadingSequence.jsx
+++ src/components/LoadingSequence.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import styles from './LoadingSequence.module.css'; // crearás este archivo
+import styles from './LoadingSequence.module.css';
 
 const SEQUENCE = [
   { cmd: true, text: 'initializing portfolio...', delay: 0 },
